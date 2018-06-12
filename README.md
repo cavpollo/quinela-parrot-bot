c7530b35ae2696f4189f83e3fe66e5cc1c6fc58c
cavpollo <cvillagran@osigu.com>
2018-06-11 18:24:29 -0600

    Request external thing

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Party Parrots in you Slack review team!
+# Quinela Party Parrot as a Service
 
 ## What's this?
 
-It's a Slack Party Parrot bot to list up review waiting list based on [the original review waiting list bot](https://github.com/ohbarye/review-waiting-list-bot), but simplified for my use case.
+It's a Slack Party Parrot bot to do "quinela" related stuff. Don't ask.
 
 ## Usage
 
@@ -15,7 +15,6 @@ In your Slack room, just call the bot.
 
 argument | presence | description
 --- | --- | ---
-organization | Required | Your organization name.
 labels | Optional | You can specify multiple labels comma separated.
 
 Also, you'll need to configure the following variables in your Heroku environment:
@@ -33,7 +32,7 @@ You can use this bot even better in combination with the Slack reminder.
 For instance, the following reminder setting invokes the bot every weekday 11 am.
 
 ```
-/remind #general “@parrotbot ls organization:"pay-code" labels:"CODE REVIEW"” at 10am every weekday
+/remind #general “@parrotbot scoreboard labels:"CODE REVIEW"” at 10am every weekday
 ```
 
 <img src="http://cultofthepartyparrot.com/parrots/hd/parrot.gif" width="32" height="32">
--- src/App.js
+++ src/App.js
@@ -2,19 +2,55 @@
 
 const SlackBot = require('./SlackBot')
 const Parser = require('./Parser')
+const https = require('https')
 
 class App {
-  static start() {
-    const controller = new SlackBot().getController()
+    static start() {
+        const controller = new SlackBot().getController()
 
-    controller.hears("scoreboard (.+)", ["direct_message", "direct_mention", "mention"], this.scoreboard)
-  }
+        controller.hears("scoreboard (.+)", ["direct_message", "direct_mention", "mention"], this.scoreboard)
+    }
 
-  static scoreboard(bot, message) {
-    const {labels} = new Parser(message.match[1]).parse()
+    static scoreboard(bot, message) {
+        const {labels} = new Parser(message.match[1]).parse()
 
-    console.log('Stuff for [Labels:' + labels.join(',') + ']!')
-  }
+        console.log('Stuff for [Labels:' + labels.join(',') + ']!')
+
+        let host = process.env.QUINELA_HOST
+        let port = process.env.QUINELA_PORT
+        let path = process.env.QUINELA_PATH
+        let token = process.env.QUINELA_TOKEN
+
+        var options = {
+            host: host,
+            port: port,
+            path: path,
+            headers: {
+                'Authorization': token
+            }
+        }
+
+        https.get(options, (resp) => {
+            let data = ''
+
+            resp.on('data', (chunk) => {
+                data += chunk
+            })
+
+            resp.on('end', () => {
+                bot.reply({channel: message.channel}, {'text': 'JELLO'})
+
+                console.log(data);
+
+                console.log('Done notifying')
+            })
+
+        }).on("error", (err) => {
+            bot.reply({channel: message.channel}, {'text': 'Error :sad_parrot:'})
+
+            console.error('Error: ' + err.message)
+        });
+    }
 }
 
 module.exports = App
